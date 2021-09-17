e87482a5ba90a3e79ff9356d53c780bdda8ee4e1
jkachmar <git@jkachmar.com>
2021-09-17 18:25:44 -0400

    Removes Nix

--- pinned.nix
+++
@@ -1,6 +0,0 @@
-import (
-  builtins.fetchTarball {
-    url = "https://github.com/nixos/nixpkgs/archive/f3df87c34eaaef618bdab64ac847512454b50285.tar.gz";
-    sha256 = "08sd2lpnzvky78fydv96zwpg4k7ms536y904aampx2qsm2rwdlgx";
-  }
-)
